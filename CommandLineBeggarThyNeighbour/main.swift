fd54d65c4f03f1c863828b2eceb36896dc49a4b8
Russell Gordon <github@russellgordon.ca>
2020-02-20 16:51:44 -0500

    Initial Commit

---
+++ CommandLineBeggarThyNeighbour/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CommandLineBeggarThyNeighbour
+//
+//  Created by Gordon, Russell on 2020-02-20.
+//  Copyright © 2020 Gordon, Russell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
